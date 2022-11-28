27e45f9dcec038d11dee94746439c63fdd6f90a4
Vicky Aryan <96813659+pwnb0y@users.noreply.github.com>
2022-11-28 10:40:40 +0530

    Update webController.java

--- src/main/java/com/securekomodo/text4shellpoc/controller/webController.java
+++ src/main/java/com/securekomodo/text4shellpoc/controller/webController.java
@@ -11,39 +11,27 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class webController {
-	
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	@ResponseBody
-	public String index() {
-		String response = "Text4Shell POC Test -@securekomodo" + "</br>"
-						+ "Send payloads to /reflected?poc=yourpayload" + "</br>"
-						+ "OR Send payloads to /blind with payload as your userAgent";
-		return response;
-	}
 
-	@RequestMapping(value = "/reflected", method = RequestMethod.GET)
-	@ResponseBody
-	public String reflected(@RequestParam(defaultValue="") String poc) {
-		StringSubstitutor interpolator = StringSubstitutor.createInterpolator();
-		try{
-			String vuln = interpolator.replace(poc);
-		} catch(Exception e) {
-			System.out.println(e);
-		}
-		return "Result: " + poc;
-	}
+        @RequestMapping(value = "/", method = RequestMethod.GET)
+        @ResponseBody
+        public String index() {
+                String response = "Hello HACK3R5" + "</br>"
+                                                + "Send your name to /fun?name=yourname" + "</br>"
+                                                + "and see the fun!";
+                return response;
+        }
 
-	@RequestMapping(value = "/blind", method = RequestMethod.GET)
-	@ResponseBody
-	public String blind(@RequestHeader(value = "User-Agent") String userAgent) {
-		StringSubstitutor interpolator = StringSubstitutor.createInterpolator();
-		try{
-			String vuln = interpolator.replace(userAgent);
-		} catch(Exception e) {
-			System.out.println(e);
-		}
-		return "Result: Thank you, your userAgent has been processed";
-	}
+        @RequestMapping(value = "/fun", method = RequestMethod.GET)
+        @ResponseBody
+        public String reflected(@RequestParam(defaultValue="pwnb0y") String name) {
+                StringSubstitutor interpolator = StringSubstitutor.createInterpolator();
+                try{
+                        String vuln = interpolator.replace(name);
+                } catch(Exception e) {
+                        System.out.println(e);
+                }
+                return "Hello " + name + "!" ;
+}
 
 
 }
